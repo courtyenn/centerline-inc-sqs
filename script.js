3e6285edfbfdb8d363b9af07b6464670748b8da8
Courtney Nguyen <cmccleve@shortcut.com>
2022-03-21 21:43:11 -0600

    Drag and drop. Intl-tel-input form styling.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /**
+ * HEAD
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.16/js/intlTelInput.min.js" integrity="sha512-Po9nSdYOcWIcoADdRjkAbRYPpR8OHjxzA/3RDUERZcDewTLzRTxbG4bUX7Sr7lVEcO3wTCzphdOBWgNFKVmxaA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.16/css/intlTelInput.css" integrity="sha512-gxWow8Mo6q6pLa1XH/CcH8JyiSDEtiwJV78E+D+QP0EVasFs8wKXq16G8CLD4CJ2SnonHr4Lm/yY2fSI2+cbmw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+*/
+
+/**
+ * This goes in the FOOTER I think
 <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+
 <script type="text/javascript">
 */
 /**
