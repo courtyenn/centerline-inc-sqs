e7d36dd944f241a215c4d9ae74311b755e493d26
Courtney Nguyen <courtney.nguyen@shortcut.com>
2022-07-02 12:48:37 -0600

    rename index.js -> pdf-viewer

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,13 @@
-const { src, dest, parallel } = require('gulp');
-const uglify = require('gulp-uglify');
-const lessify = require('gulp-less');
-const path = require('path');
+const { src, dest, parallel } = require("gulp");
+const uglify = require("gulp-uglify");
+const lessify = require("gulp-less");
 
 function buildJS() {
-  // place code for your default task here
-
-  return src('index.js')
-    .pipe(uglify())
-    .pipe(dest('build/'))
+  return src("index.js").pipe(uglify()).pipe(dest("build/"));
 }
 
 function buildCSS() {
-  return src('./custom.less')
-    .pipe(lessify())
-    .pipe(dest('build/'));
+  return src("./custom.less").pipe(lessify()).pipe(dest("build/"));
 }
-
 
 exports.default = parallel([buildJS, buildCSS]);
