7b5df0b941a478aca38e837133ebb4a42959ce55
Courtney Nguyen <courtney.nguyen@shortcut.com>
2022-10-29 23:48:59 -0600

    slideshow style

--- src/contact-us/index.ts
+++ src/contact-us/index.ts
@@ -1,8 +1,32 @@
+const telInput = document.querySelector("#phone");
+const submit = document.getElementById("submit");
+const iti = window.intlTelInput(telInput, {
+  // any initialisation options go here
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.16/js/utils.min.js",
+  preferredCountries: ["US"],
+  nationalMode: false,
+  formatOnDisplay: true, // SET THIS!!!
+});
+
+telInput.addEventListener("keyup", formatIntlTelInput);
+telInput.addEventListener("change", formatIntlTelInput);
+
+function formatIntlTelInput() {
+  if (typeof intlTelInputUtils !== "undefined") {
+    // utils are lazy loaded, so must check
+    var currentText = iti.getNumber(intlTelInputUtils.numberFormat.E164);
+    if (typeof currentText === "string") {
+      // sometimes the currentText is an object :)
+      iti.setNumber(currentText); // will autoformat because of formatOnDisplay=true
+    }
+  }
+}
+
 /** File Upload **/
-(function ($, window, document) {
+(function ($, window, document, undefined) {
   // feature detection for drag&drop upload
 
-  const _files: File[] = [];
+  const _files = [];
   var isAdvancedUpload = (function () {
     var div = document.createElement("div");
     return (
@@ -12,18 +36,24 @@
     );
   })();
 
-  const appendFileThumbnail = (file: File, appendTo) => {
+  const appendFileThumbnail = (file, appendTo) => {
     _files.push(file);
     const newFile = document.createElement("img");
-    const src = URL.createObjectURL(file);
-    newFile.src = src;
 
     const contentSpan = document.createElement("span");
     const newContent = document.createTextNode(file.name);
     contentSpan.append(newContent);
 
     const thumbnail = document.createElement("div");
-    thumbnail.className = "thumbnail";
+    const classList = "thumbnail";
+    if (file.type.includes("pdf")) {
+      classList += " pdf";
+      newFile.src = "https://centerline-inc.s3.us-west-1.amazonaws.com/dist/static/pdf-icon.png";
+    } else {
+      const src = URL.createObjectURL(file);
+      newFile.src = src;
+    }
+    thumbnail.classList = classList;
 
     thumbnail.append(contentSpan);
     thumbnail.append(newFile);
@@ -36,15 +66,25 @@
     $label = $form.find(".file__label"),
     $errorMsg = $form.find(".box__error span"),
     $restart = $form.find(".box__restart"),
+    $errorBox = $form.find(".error-box"),
     $box = $form.find(".box");
 
   let droppedFiles = false;
 
   const showFiles = function (files) {
-    console.log("files", files);
+    // console.log("files", files);
     _files.length === 0 && $label.html("");
-    files.forEach((file) => appendFileThumbnail(file, $label));
+    Array.from(files).forEach((file) => appendFileThumbnail(file, $label));
+    // $label.text(files.length > 1 ? ($input.attr('data-multiple-caption') || '').replace('{count}', files.length) : files[0].name);
   };
+
+  const showError = (name) => {
+    $errorBox.addClass(`${name}`);
+  };
+
+  $form.on("change", () => {
+    $errorBox.removeClass("required");
+  });
 
   $input.on("change", function (e) {
     showFiles(e.target.files);
@@ -88,19 +128,31 @@
     });
 
   const submitResponse = document.querySelector("#response");
-  const formURL = "http://localhost:3000/upload";
+  const formURL = "https://8c4b8geecl.execute-api.us-west-1.amazonaws.com/Prod/CenterLineFormSubmission";
 
   $form.on("submit", (e) => {
     e.preventDefault();
     const formData = new FormData($form.get(0));
 
-    if (droppedFiles) {
-      $.each(droppedFiles, function (i, file) {
-        formData.append($input.attr("name"), file);
-      });
+    if (
+      !formData.get("name") ||
+      !formData.get("message") ||
+      !formData.get("email") ||
+      !formData.get("phone") ||
+      !formData.get("reason")
+    ) {
+      showError("required");
+      return;
     }
 
-    console.log("Sending: ", formData);
+    if (droppedFiles) {
+      if (droppedFiles)
+        $.each(droppedFiles, function (i, file) {
+          formData.set($input.attr("name"), file);
+        });
+    }
+
+    console.log("Sending: ", formData.get("files[]"));
 
     $.ajax({
       url: formURL,
