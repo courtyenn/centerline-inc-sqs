a9374ceb4d9feeca5d5f9375521741874263b56f
Courtney Nguyen <courtney.nguyen@shortcut.com>
2023-02-18 21:04:09 -0700

    Fixing pdf viewer - inline view

--- src/pdf-viewer/index.js
+++ src/pdf-viewer/index.js
@@ -3,10 +3,10 @@
 */
 document.addEventListener("adobe_dc_view_sdk.ready", function()
 {
-    var adobeDCView = new AdobeDC.View({clientId: "1900c81d5da546a999b6a4aa772049fd", divId: "adobe-dc-view"});
+    const adobeDCView = new AdobeDC.View({clientId: "1900c81d5da546a999b6a4aa772049fd", divId: "adobe-dc-view"});
     adobeDCView.previewFile(
     {
-      content:   {location: {url: "https://centerline-inc.s3.us-west-1.amazonaws.com/spindle_repair_information.pdf"}},
+      content: {location: {url: "https://centerline-inc.s3.us-west-1.amazonaws.com/spindle_repair_information.pdf"}},
       metaData: {fileName: "SpindleRepairIntakeForm.pdf"}
-    });
+    }, {embedMode: "IN_LINE"});
 });
